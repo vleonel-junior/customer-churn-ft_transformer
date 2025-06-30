f7d9ca3c7e16c633bcbf9fb9ea10cdf8f9681995
Léonel VODOUNOU <vleoneljunior@gmail.com>
2025-06-30 21:12:08 +0100

    v.7.5.5.1

--- train/Telecom/train_ftt/train.py
+++ train/Telecom/train_ftt/train.py
@@ -44,8 +44,8 @@ if __name__ == '__main__':
 
     d_embedding = model.feature_tokenizer.d_token
 
-    # Embedding numérique personnalisé : LR
-    embedding_type = "LR"  # Choisir le type d'embedding numérique
+    # Embedding numérique personnalisé : Q-LR
+    embedding_type = "Q-LR"  # Choisir le type d'embedding numérique
     print(f"Type d'embedding numérique: {embedding_type}")
 
     num_embedding = get_num_embedding(
